4bf41024f50570e54e358ba107d8b07da7f45d3f
MAK-TommyPan <137017481+MAK-TommyPan@users.noreply.github.com>
2023-07-24 18:21:48 +0800

    commit
    commit

---
+++ local/index.php
@@ -0,0 +1,179 @@
+<?php include "quote/template/head.php"; ?>
+<link rel="stylesheet" href="dist/css/index.css">
+
+</head>
+
+<body class="lang_tw" data-menu="0">
+    <?php
+    include "quote/template/added.php";
+    include "quote/template/nav.php";
+    ?>
+    <main>
+        <div class="banner">
+            <!-- PC : 2400*1500px -->
+            <!-- MB : 740*1400px  -->
+            <div class="top_logo">
+                <img src="dist/images/evolgear-logo_fromjp.png" alt="EVOLGEAR from JAPAN">
+            </div>
+            <div class="olympia-logo">
+                <div>
+                    <img src="dist/images/olympia-logo.svg">
+                </div>
+                <p class="olympia-text">EVOLGEAR是國際健美大賽“OLYMPIA”的官方認定訓練機械品牌</p>
+            </div>
+        </div>
+
+        <section class="bg_black">
+            <div class="category-link-btn">
+                <ul>
+                    <li><a href="professional/index.php">Professional Edition</a></li>
+                    <li><a href="lp/homegym.php">Home Edition</a></li>
+                    <li><a href="corerevo/index.php">Pilates Series</a></li>
+                </ul>
+            </div>
+
+            <div class="official-sponser-wrapper">
+                <p class="title_en">OFFICIAL SPONSOR</p>
+                <span class="title_line">_</span>    
+
+                <div class="editor_content">
+                    <div class="editor_box pc_use">這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 </div>
+                    <div class="editor_box mo_use">這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 </div>
+                </div>
+            </div>
+        </section>
+            
+        <section class="section2">
+            <div class="editor_content">
+                <div class="editor_box pc_use">這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 這是桌機 </div>
+                <div class="editor_box mo_use">這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 這是手機 </div>
+            </div>
+        </section>
+
+        <section class="bg_black-2">
+            <p class="title_en">PRODUCT</p>
+            <span class="title_line">_</span>  
+
+            <div class="add-top-btn">
+                <a href="professional/index.php" btn_click_track="hp_pro_edition">
+                    <div class="top-btn-shadow">
+                        <!-- 1000*496px -->
+                    <img src="dist/images/pro-ed.jpg" class="ed-img">
+                    <div class="ed-tit">
+                        <div class="ed-tit-cat">Professional Edition</div>
+                        <div class="ed-tit-caption">查看業務用健身器材</div>
+                    </div>
+                    </div>
+                </a>
+                <a href="lp/homegym.php" btn_click_track="hp_home_edition">
+                    <div class="top-btn-shadow">
+                        <!-- 1000*496px -->
+                    <img src="dist/images/home-ed.jpg" class="ed-img">
+                    <div class="ed-tit">
+                        <div class="ed-tit-cat">Home Edition</div>
+                        <div class="ed-tit-caption">查看家庭用健身器材</div>
+                    </div>
+                    </div>
+                </a>
+                <a href="corerevo/index.php" btn_click_track="hp_corerevo">
+                    <div class="top-btn-shadow">
+                        <!-- 1000*496px -->
+                    <img src="dist/images/pilates-ed.jpg" class="ed-img">
+                    <div class="ed-tit tit-pilates">
+                        <div class="ed-tit-cat">Pilates Series</div>
+                        <div class="ed-tit-caption">查看皮拉提斯器材</div>
+                    </div>
+                    </div>
+                </a>
+            </div>
+
+            <article>
+                <p class="title_en">TOPICS</p>
+                <span class="title_line">_</span> 
+
+                <div class="row top-topics-area">
+                    <div class="col topics5">
+                        <a href="https://evolgear.com.tw/syn_news/202210_agreement.php" btn_click_track="hp_home_showroom">
+                            <div>
+                                <!-- 480*480px -->
+                                <img src="dist/images/a.jpg" alt="">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col topics4">
+                        <a href="showroom.php" btn_click_track="hp_home_showroom">
+                            <div>
+                                <!-- 480*480px -->
+                                <img src="dist/images/b.jpg" alt="">
+                            </div>
+                        </a>
+                    </div> 
+                    <div class="col topics2">
+                        <a href="casestudy.php" btn_click_track="hp_home_casestudy">
+                            <div>
+                                <!-- 480*480px -->
+                                <img src="dist/images/c.jpg" alt="">
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col topics1">
+                        <a href="merit.php" btn_click_track="hp_home_merit">
+                            <div>
+                                <!-- 480*480px -->
+                                <img src="dist/images/d.jpg" alt="">
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </article>
+        </section> 
+
+        <section class="part-3">
+            <p class="title_en_bk">NEWS</p>
+            <span class="title_line">_</span>    
+            <h2 class="read_jp_bk">最新消息</h2>
+            <ul class="news_list">      
+                <a href="./news.php">
+                    <li><span class="date">2023.5.15</span><span class="title">日本專業健身器材品牌EVOLGEAR即將登陸台灣了！</span></li>
+                </a>
+                <a href="./news.php">
+                    <li><span class="date">2023.5.15</span><span class="title">日本專業健身器材品牌EVOLGEAR即將登陸台灣了！</span></li>
+                </a>
+                <a href="./news.php">
+                    <li><span class="date">2023.5.15</span><span class="title">日本專業健身器材品牌EVOLGEAR即將登陸台灣了！</span></li>
+                </a>
+            </ul>
+            <div class="readmore">
+                <a href="news.php">read more <img src="dist/images/news_arrow.svg"></a>
+            </div>
+        </section>
+        
+        <!-- <div class="corner">
+            <ul>
+                <li>
+                    
+                    <ul>
+                        <li><a href=""><div class="sidebar-circle contact"><i class="fa-solid fa-phone"></i></div></a></li>
+                        <li><a href=""><div class="sidebar-circle contact"><i class="fa-brands fa-line"></i></div></a></li>
+                        <li><a href=""><div class="sidebar-circle contact"><i class="fa-brands fa-facebook-messenger"></i></div></a></li>
+                    </ul>
+                    <a href="">
+                        <div class="sidebar-circle main"><i class="fa-regular fa-comment"></i></div>
+                    </a>
+                </li>
+            </ul>
+        </div> -->
+        <?php
+            include "quote/template/sidebar.php";
+        ?>
+    </main>
+    <?php
+    include "quote/template/footer.php";
+    // include "quote/template/top_btn.php";
+    ?>
+    <script src="dist/js/jquery_min.js"></script>
+    <script src="dist/js/function.js"></script>
+    <!-- <script src="dist/js/index.js"></script> -->
+</body>
+
+</html>
